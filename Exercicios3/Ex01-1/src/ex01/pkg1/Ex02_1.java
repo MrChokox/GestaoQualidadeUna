4e779bece4415112835a99711003c196895c8235
MrChokox <juniojms13@gmail.com>
2022-03-30 20:54:46 -0300

    Exercicio 3 de 1 a 6

---
+++ Exercicios3/Ex01-1/src/ex01/pkg1/Ex02_1.java
@@ -0,0 +1,24 @@
+
+package ex01.pkg1;
+
+import java.util.Scanner;
+
+public class Ex02_1 {
+
+    public static void main(String[] args) {
+        int num1,num2,num3;        
+        Scanner sc = new Scanner(System.in);        
+        
+        System.out.println("Digite 3 valores");
+        System.out.println("Valor 1");
+        num1 = sc.nextInt();
+        System.out.println("Valor 2");
+        num2 = sc.nextInt();
+        System.out.println("Valor 3");
+        num3 = sc.nextInt();
+        
+        double total = (num1 + num2 + num3)/3;
+        System.out.println("A média é " + total);
+    }
+    
+}
