6740e76633d70080216a94e0b5b2bc0a3de7a043
MrChokox <juniojms13@gmail.com>
2022-04-06 21:40:48 -0300

    Exercicio 04 de 2 a 4

--- Exercicios4/Ex04/src/ex04/atv4.java
+++ Exercicios4/Ex04/src/ex04/atv4.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ex04;
 
-/**
- *
- * @author Gualberto
- */
+import java.util.Scanner;
+
 public class atv4 {
-    
+    public static void main(String[] args) {
+        double altura,peso,imc;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bem vindo(a) ao meu programa para verificar seu peso\nMe informe seu peso atual: ");
+        peso = sc.nextDouble();
+        System.out.println("Ok, agora digite sua altura: ");
+        altura = sc.nextDouble();
+        imc = peso/(altura*altura);
+        if(imc < 20)
+            System.out.println("Você está abaixo do peso");
+        else if(imc <= 25)
+            System.out.println("Peso Normal");
+        else if(imc <=30)
+            System.out.println("Você está com sobre peso");
+        else if(imc <= 40)
+            System.out.println("Você está Obeso");
+        else if(imc > 40)
+            System.out.println("Você está com obesidade morbida");
+       
+    }
 }
