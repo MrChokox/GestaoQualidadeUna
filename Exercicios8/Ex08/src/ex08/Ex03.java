e23b13f2e8aa19908a56d374067a4ddfc8aeaf64
Gualberto Junio Moreira Silva - 42113921 <juniojms13@gmail.com>
2022-04-19 20:19:09 -0300

    Exercicio 8 atividade 2

--- Exercicios8/Ex08/src/ex08/Ex03.java
+++ Exercicios8/Ex08/src/ex08/Ex03.java
@@ -1,26 +1,33 @@
 package ex08;
-
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Ex02 {
+public class Ex03 {
     public static void main(String[] args) {
         //Leia 10 números reais a partir do teclado e os armazene em um vetor. O algoritmo deve
-        //imprimir o vetor e as posições do vetor que armazenam números negativos.
+        //imprimir o vetor, o menor e o maior número informado.
         Scanner sc = new Scanner(System.in);
         int[] vetor = new int[10];
+        int maior = 0, menor = 0;
         
         for (int i = 0; i < vetor.length; i++) {
             System.out.println("Informe o " + i + "º numero:");
             int num = sc.nextInt();
             vetor[i] = num;            
-        }  
-        System.out.println("==================================");
-        System.out.println("Vetor: " + Arrays.toString(vetor));
+            if(i == 0){
+                maior = num;
+                menor = num;
+            }
+            if(num > maior)
+                maior = num;
+            if(num < menor)
+                menor = num;
+        }
+//        System.out.println("Vetor: " + Arrays.toString(vetor));
         for (int i = 0; i < vetor.length; i++) {
             if(vetor[i] < 0)
                 System.out.println("Posição: " + i + " valor: " + vetor[i]);
         }
-        System.out.println("==================================");
+        System.out.println("O maior número é: " + maior + " e o menor valor é: " + menor);
     }
 }
