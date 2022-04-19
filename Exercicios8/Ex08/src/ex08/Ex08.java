7eb9cf227e707b15214420cccc29dcfd38b52aab
Gualberto Junio Moreira Silva - 42113921 <juniojms13@gmail.com>
2022-04-19 19:23:22 -0300

    Exercicios 8

---
+++ Exercicios8/Ex08/src/ex08/Ex08.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex08;
+
+/**
+ *
+ * @author 42113921
+ */
+public class Ex08 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
